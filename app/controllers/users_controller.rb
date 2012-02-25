4ff961523c83bb3e4fe98c64d6f9ec45fbf7fc86
Leon Kay <leokay@gmail.com>
2012-02-25 18:44:10 -0500

    users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
   def new
-    @user = User.create(:email => params[:email], :name => params[:name])
+    args = base64_url_decode(params[:signed_request])
+    @user = User.create(:email => args[:email], :name => args[:name])
   end
+private
+
+  def base64_url_decode(str)
+   str += '=' * (4 - str.length.modulo(4))
+   Base64.decode64(str.tr('-_','+/'))
+ end
 
 end
