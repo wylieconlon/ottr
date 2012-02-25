45aa39dcffb4494a7672d4bea547e15a4794f871
Leon Kay <leokay@gmail.com>
2012-02-25 16:46:14 -0500

    model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,7 @@ class User < ActiveRecord::Base
   validates :email, :presence => true,
                     :format =>  { :with => email_regex },
                     :uniqueness => { :case_sensitive => false }
+                    
+  # test
   
 end
