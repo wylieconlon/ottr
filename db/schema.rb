8a677ad0e1dac0e5b0f70c821cf054e3160b4ea2
Wylie Conlon <wylieconlon@gmail.com>
2012-02-25 22:14:59 -0500

    Updated migration to properly do many-many relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120226012916) do
+ActiveRecord::Schema.define(:version => 20120226030945) do
 
   create_table "ideas", :force => true do |t|
     t.string   "what"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(:version => 20120226012916) do
     t.string   "who"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "ideas_users", :id => false, :force => true do |t|
+    t.integer "idea_id"
+    t.integer "user_id"
   end
 
   create_table "users", :force => true do |t|
