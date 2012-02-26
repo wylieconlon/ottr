859875fe5109988986b9ebc610db407a4d7bcbed
Leon Kay <leokay@gmail.com>
2012-02-26 08:21:01 -0500

    8:20

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 OttrApp::Application.routes.draw do
   resources :ideas
   resources :comments, :only => [:create]
-  resources :invites, :only => [:create]
+  resources :invites, :only => [:update]
   
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   
