a559fd0bad8e5fabd2d3428f8f6870f7b0f75eaf
Leon Kay <leokay@gmail.com>
2012-02-25 19:16:31 -0500

    logging

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,10 @@ class SessionsController < ApplicationController
   end
   
   def fblogin
-    arguments = FacebookAuthentication::parse_signed_request(params[:signed_request], 'e046639ad76c13c08ec696376e535062')
+    @app_key = "e046639ad76c13c08ec696376e535062"
+    arguments = FacebookAuthentication::parse_signed_request(params[:signed_request], @app_key)
+    puts "Facebook Parameters: #{arguments}"
+    
     @user = User.find_by_email(@arguments[:email])
     if @user.nil?
       redirect_to 'signup', :email => arguments[:email], :name => arguments[:name]
